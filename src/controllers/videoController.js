ccf132eac5c5db58f2160faaa23d51f941d7dbaf
Ku <bonsung1114@icloud.com>
2022-01-27 16:38:40 +0900

    Stlye: modify templates using javascript Use Javascript feature to make more easier to manage templates.

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,36 +1,39 @@
+let videos = [
+  {
+    title: "Video #1",
+    rating: 5,
+    comments: 2,
+    createdAt: "2 minutes ago",
+    views: 59,
+    id: 1,
+  },
+  {
+    title: "Video #2",
+    rating: 5,
+    comments: 2,
+    createdAt: "2 minutes ago",
+    views: 59,
+    id: 2,
+  },
+  {
+    title: "Video #3",
+    rating: 5,
+    comments: 2,
+    createdAt: "2 minutes ago",
+    views: 59,
+    id: 3,
+  },
+];
+
 export const recommended = (req, res) => {
-  const videos = [
-    {
-      title: "Video #1",
-      rating: 5,
-      comments: 2,
-      createdAt: "2 minutes ago",
-      views: 59,
-      id: 1,
-    },
-    {
-      title: "Video #2",
-      rating: 5,
-      comments: 2,
-      createdAt: "2 minutes ago",
-      views: 59,
-      id: 1,
-    },
-    {
-      title: "Video #3",
-      rating: 5,
-      comments: 2,
-      createdAt: "2 minutes ago",
-      views: 59,
-      id: 1,
-    },
-  ];
   return res.render("home", { pageTitle: "Home", videos });
   //send to base.pug #{pageTitle} and render Home.pug
 };
 
-export const see = (req, res) => {
-  res.render("watch");
+export const watch = (req, res) => {
+  const { id } = req.params;
+  const video = videos[id - 1];
+  res.render("watch", { pageTitle: `Watching ${video.title}`, video });
 };
 
 export const edit = (req, res) => {
