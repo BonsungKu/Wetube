661b78acf0caca471d37fe6d450c7ccbe1ad72e5
Ku <bonsung1114@icloud.com>
2022-02-28 19:15:45 +0900

    Add [trim] on VideoJs and max/minLength on VideoJs and uploadPug file Add [trim] and length conditions to title and description. Also, add same length conditions to upload.pug html template. Delete unnecessary codes of video.pug which is before use DB. And add some codes for show the information of videos array.(temporary)

--- src/models/Video.js
+++ src/models/Video.js
@@ -3,18 +3,17 @@ import mongoose from "mongoose";
 //creating video model
 //Schema를 사용하여 데이터베이스의 구조와 제약조건을 설정
 const videoSchema = new mongoose.Schema({
-  title: { type: String, required: true },
+  title: { type: String, required: true, trim: true, maxlength: 80 },
   //=={type: String}
-  description: { type: String, required: true },
+  description: { type: String, required: true, trim: true, minlength: 20 },
   //default값을 설정함으로서 매번 모델작성시에 상세부분까지 입력하지않아도 됨.
   createdAt: { type: Date, required: true, default: Date.now },
-  hashtags: [{ type: String }],
+  hashtags: [{ type: String, trim: true }],
   meta: {
     views: { type: Number, default: 0, required: true },
     rating: { type: Number, default: 0, required: true },
   },
 });
-
 //model 생성
 const Video = mongoose.model("Video", videoSchema);
 export default Video;
